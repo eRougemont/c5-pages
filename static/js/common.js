e1f9456de1bd7b01ed805c97c9c4ae123fdaa933
Frédéric Glorieux <frederic.glorieux@fictif.org>
2019-07-02 13:00:40 +0200

    Première version du site

---
+++ static/js/common.js
@@ -0,0 +1,34 @@
+/**
+ * For dev only, so that no technologies assumptions are yet made
+ */
+function clientSideInclude(id, url) {
+  var req = false;
+  // W3C browsers
+  if (window.XMLHttpRequest) {
+    try {
+      req = new XMLHttpRequest();
+    } catch (e) {
+      req = false;
+    }
+  } // still old IE around ?
+  else if (window.ActiveXObject) {
+    try {
+      req = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {
+        req = false;
+      }
+    }
+  }
+  var element = document.getElementById(id);
+  if (!element) return; // bad id
+  if (!req) return; // old browser
+  // Synchronous request, wait till we have it all
+  req.open('GET', url, false);
+  req.send(null);
+  element.innerHTML = req.responseText;
+}
+clientSideInclude("top", "inc/top.html");
+clientSideInclude("bottom", "inc/bottom.html");
