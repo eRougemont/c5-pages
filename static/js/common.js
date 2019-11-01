e614a4cef006f9c40ceb5ea9f9d061247dfa92e4
Frédéric Glorieux <frederic.glorieux@fictif.org>
2019-11-01 10:48:43 +0100

    nouvelles ressources

--- static/js/common.js
+++ static/js/common.js
@@ -1,24 +1,27 @@
 /**
  * For dev only, so that no technologies assumptions are yet made
  */
-function clientSideInclude(id, url) {
-  var element = document.getElementById(id);
-  if (!element) return; // bad id
+function clientSideInclude(id, url, up) {
+  var cont = document.getElementById(id);
+  if (!cont) return; // bad id
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url);
   xhr.send(null);
   xhr.responseType = 'text';
   xhr.onload = function() {
     let html = xhr.response;
-    element.innerHTML = html;
-    if (!relup) return;
-    const links = element.getElementsByTagName('a');
-    for (var i = 0, len = links.length; i < len; i++) {
-      let a = links[i];
-      let href = a.getAttribute("href");
-      if (!href) continue;
-      if (href.startsWith('/') || href.startsWith('http')) continue;
-      a.setAttribute("href", relup+href);
+    cont.innerHTML = html;
+    var els = cont.querySelectorAll('a, img')
+    for (var i = 0, len = els.length; i < len; i++) {
+      let el = els[i];
+      let url;
+      let attName;
+      if (el.href) attName = "href";
+      else if (el.src) attName = "src";
+      else continue;
+      let link = el.getAttribute(attName);
+      if (link.startsWith('/') || link.startsWith('http')) continue;
+      el.setAttribute(attName, up+link);
     }
   };
 }
@@ -28,6 +31,7 @@ var path = window.location.pathname;
 const ancestors = 1;
 let depth = path.split("/").length - 2 - ancestors;
 let up = "../".repeat(depth);
+console.log(up);
 
-clientSideInclude("top", up+"inc/tete.html");
-clientSideInclude("bottom", up+"inc/pied.html");
+clientSideInclude("top", up+"inc/tete.html", up);
+clientSideInclude("bottom", up+"inc/pied.html", up);
